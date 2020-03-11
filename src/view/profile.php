99642d78d8326e0781f89c71316ccc68b17c3e55
Lisa Servais <lisa.servais@hotmail.com>
2020-03-11 10:17:00 +0100

    style login

--- src/view/profile.php
+++ src/view/profile.php
@@ -14,13 +14,13 @@ logout();
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="../public/css/profile.scss"> 
+    <link rel="stylesheet" type="text/css" href="../public/css/style.css"> 
      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
   <link href="../public/css/emoji.css" rel="stylesheet">
 </head>
 
 <body>
-    <?php require("../view/navbar.php"); ?>
+    <?php require("./navbar.php"); ?>
     <div id="profile">
 
         <div class="container">
