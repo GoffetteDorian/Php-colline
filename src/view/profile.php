458824e568c3fb7260d12a523967576f94e25d93
GoffetteDorian <goffettedorian@gmail.com>
2020-03-11 09:23:44 +0100

    navbar in profile fix

--- src/view/profile.php
+++ src/view/profile.php
@@ -9,16 +9,13 @@ logout();
 
 <head>
     <title>Profile</title>
-
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="/public/css/profile.css">
-    <link rel="stylesheet" href="text/css" href="/public/css/style.css"> 
+    <link rel="stylesheet" type="text/css" href="/public/css/style.css"> 
 </head>
 
 <body>
-    <?php require("./navbar.php"); ?>
+    <?php require("../view/navbar.php"); ?>
     <div id="profile">
 
         <div class="container">
