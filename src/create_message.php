bfe15e1b4864b6d774d1fcbd6f5cf8f50bcc3869
GoffetteDorian <goffettedorian@gmail.com>
2020-03-11 09:47:35 +0100

    discovered a new bug in message creation (create_message.php)

--- src/create_message.php
+++ src/create_message.php
@@ -8,10 +8,11 @@
   $parse = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_QUERY);
   parse_str($parse, $queries);
 
+  echo $_SESSION["email"];
   $idUser = getUserIdByEmail($pdo, $_SESSION["email"]);
+  
   $topic = getCurrentTopic($pdo, $queries["topic"]);
 
-  echo $idUser;
 
   $sql = 'INSERT INTO messages (content, creation_date, users_idusers, topics_idtopics)
           VALUES ("' . $_POST["content"] . '", CURRENT_TIMESTAMP, ' . $idUser . ', ' . $topic["idtopics"] . ')';
