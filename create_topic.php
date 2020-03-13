4bd56f4915edaa3ca8a8abf6bf13c0cd31db2a4d
GoffetteDorian <goffettedorian@gmail.com>
2020-03-13 12:19:15 +0100

    heroku deploy tests

--- create_topic.php
+++ create_topic.php
@@ -1,22 +1,22 @@
 <?php
 session_start();
-require_once("config.php");
-$pdo = db_connect();
+// require_once("config.php");
+// $pdo = db_connect();
 
-require_once("./model/model.php");
+// require_once("./model/model.php");
 
-$parse = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_QUERY);
-parse_str($parse, $queries);
+// $parse = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_QUERY);
+// parse_str($parse, $queries);
 
-$idUser = $_SESSION["user"]["idusers"];
-$idBoard = getBoardByName($pdo, $queries["board"])["idboards"];
-$title = $_POST["title"];
+// $idUser = $_SESSION["user"]["idusers"];
+// $idBoard = getBoardByName($pdo, $queries["board"])["idboards"];
+// $title = $_POST["title"];
 
-$sql = 'INSERT INTO topics (title, creation_date, users_idusers, boards_idboards)
-          VALUES ("' . $title . '" , CURRENT_TIMESTAMP, ' . $idUser . ', ' . $idBoard . ')';
-$sth = $pdo->prepare($sql);
-$sth->execute();
-$sth->closeCursor();
+// $sql = 'INSERT INTO topics (title, creation_date, users_idusers, boards_idboards)
+//           VALUES ("' . $title . '" , CURRENT_TIMESTAMP, ' . $idUser . ', ' . $idBoard . ')';
+// $sth = $pdo->prepare($sql);
+// $sth->execute();
+// $sth->closeCursor();
 
   //redirect to previous url
   // header('Location: ' . $_SERVER['HTTP_REFERER']);
