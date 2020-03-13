a8e674ed4bc46e3586664182ae4715b246e3969c
GoffetteDorian <goffettedorian@gmail.com>
2020-03-13 12:28:26 +0100

    heroku deploy tests

--- create_topic.php
+++ create_topic.php
@@ -3,14 +3,14 @@ session_start();
 require_once("config.php");
 $pdo = db_connect();
 
-// require_once("./model/model.php");
+require_once("./model/model.php");
 
-// $parse = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_QUERY);
-// parse_str($parse, $queries);
+$parse = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_QUERY);
+parse_str($parse, $queries);
 
-// $idUser = $_SESSION["user"]["idusers"];
-// $idBoard = getBoardByName($pdo, $queries["board"])["idboards"];
-// $title = $_POST["title"];
+$idUser = $_SESSION["user"]["idusers"];
+$idBoard = getBoardByName($pdo, $queries["board"])["idboards"];
+$title = $_POST["title"];
 
 // $sql = 'INSERT INTO topics (title, creation_date, users_idusers, boards_idboards)
 //           VALUES ("' . $title . '" , CURRENT_TIMESTAMP, ' . $idUser . ', ' . $idBoard . ')';
