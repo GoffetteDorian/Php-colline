21a6b33710dd1255823b4890db48a7a0b71365d4
GoffetteDorian <goffettedorian@gmail.com>
2020-03-13 12:31:35 +0100

    heroku deploy tests

--- create_topic.php
+++ create_topic.php
@@ -12,11 +12,12 @@ $idUser = $_SESSION["user"]["idusers"];
 $idBoard = getBoardByName($pdo, $queries["board"])["idboards"];
 $title = $_POST["title"];
 
-// $sql = 'INSERT INTO topics (title, creation_date, users_idusers, boards_idboards)
-//           VALUES ("' . $title . '" , CURRENT_TIMESTAMP, ' . $idUser . ', ' . $idBoard . ')';
-// $sth = $pdo->prepare($sql);
-// $sth->execute();
-// $sth->closeCursor();
+$sql = 'INSERT INTO topics (title, creation_date, users_idusers, boards_idboards)
+          VALUES ("' . $title . '" , CURRENT_TIMESTAMP, ' . $idUser . ', ' . $idBoard . ')';
+
+$sth = $pdo->prepare($sql);
+$sth->execute();
+$sth->closeCursor();
 
   //redirect to previous url
   // header('Location: ' . $_SERVER['HTTP_REFERER']);
