d833eb1e8fd20c94e1ae8dada2b8f7ac1973b515
GoffetteDorian <goffettedorian@gmail.com>
2020-03-13 14:01:30 +0100

    heroku deploy tests

--- create_message.php
+++ create_message.php
@@ -1,17 +1,17 @@
 <?php
 session_start();
-// require_once("config.php");
-// $pdo = db_connect();
+require_once("config.php");
+$pdo = db_connect();
 
-// require_once("./model/model.php");
+require_once("./model/model.php");
 
-// $parse = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_QUERY);
-// parse_str($parse, $queries);
+$parse = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_QUERY);
+parse_str($parse, $queries);
 
-// echo $_SESSION["email"];
-// $idUser = getUserIdByEmail($pdo, $_SESSION["email"]);
+echo $_SESSION["email"];
+$idUser = getUserIdByEmail($pdo, $_SESSION["email"]);
 
-// $topic = getCurrentTopic($pdo, $queries["topic"]);
+$topic = getCurrentTopic($pdo, $queries["topic"]);
 
 
 $sql = 'INSERT INTO messages (content, creation_date, users_idusers, topics_idtopics, deleted)
@@ -29,3 +29,4 @@ echo "</pre>";
 
   //redirect to previous url
  // goToURL($_SERVER['HTTP_REFERER']);  
+?>
