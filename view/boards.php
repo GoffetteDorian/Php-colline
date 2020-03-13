ed475af4862e355d2e811b30c7c018c7d490e91d
GoffetteDorian <goffettedorian@gmail.com>
2020-03-13 16:12:07 +0100

    Fixed topic

--- view/boards.php
+++ view/boards.php
@@ -17,10 +17,6 @@
         <li class="list-group-item">
           <ul class="list-group">
             <?php $topics = getLatestTopics($pdo, $board["idboards"]); ?>
-            <?php 
-        echo "<pre>";
-        print_r($topics);
-        echo "</pre>"; ?>
             <?php foreach($topics as $topic){ ?>
               <a class="nav-link" href="index.php?board=<?php echo $board["name"]; ?>&topic=<?php echo $topic["title"]; ?>">
                 <h4><img src="../public/img/bubble.svg"><?php echo $topic["title"] ?></h4>
@@ -67,8 +63,6 @@ else{
   <ul class="list-group">
     <li class="list-group-item">
       <?php $topics = getCurrentTopics($pdo, $_GET["board"]); ?>
-      
-      
       <?php foreach($topics as $topic){ ?>
               <a class="nav-link" href="index.php?board=<?php echo $_GET["board"]; ?>&topic=<?php echo $topic["title"]; ?>">
                 <h4><img src="../public/img/bubble.svg"><?php echo $topic["title"]; ?></h4>
