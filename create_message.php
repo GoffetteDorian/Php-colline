2e3181361da5ab31941c00a7e020c841fba7e6b1
GoffetteDorian <goffettedorian@gmail.com>
2020-03-13 13:53:23 +0100

    heroku deploy tests

--- create_message.php
+++ create_message.php
@@ -1,26 +1,31 @@
 <?php
-  session_start();
-  require_once("config.php");
-  $pdo = db_connect(); 
+session_start();
+require_once("config.php");
+$pdo = db_connect();
 
-  require_once("./model/model.php");
+require_once("./model/model.php");
 
-  $parse = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_QUERY);
-  parse_str($parse, $queries);
+$parse = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_QUERY);
+parse_str($parse, $queries);
 
-  echo $_SESSION["email"];
-  $idUser = getUserIdByEmail($pdo, $_SESSION["email"]);
-  
-  $topic = getCurrentTopic($pdo, $queries["topic"]);
+echo $_SESSION["email"];
+$idUser = getUserIdByEmail($pdo, $_SESSION["email"]);
+
+$topic = getCurrentTopic($pdo, $queries["topic"]);
 
 
 $sql = 'INSERT INTO messages (content, creation_date, users_idusers, topics_idtopics, deleted)
           VALUES ("' . $_POST["content"] . '", CURRENT_TIMESTAMP, ' . $idUser . ', ' . $topic["idtopics"] . ', FALSE)';
 
-  $sth = $pdo->prepare($sql);
-  $sth->execute();
-  $sth->closeCursor(); 
+echo $sql;
+
+echo "<pre>";
+echo $_SESSION;
+echo "</pre>";
+
+// $sth = $pdo->prepare($sql);
+// $sth->execute();
+// $sth->closeCursor(); 
 
   //redirect to previous url
-  goToURL($_SERVER['HTTP_REFERER']);  
-?>
+ // goToURL($_SERVER['HTTP_REFERER']);  
